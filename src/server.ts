637e84d6a91840b8b339e757495cdfd0e116a9d0
Gabriel <gabrielbarth98@gmail.com>
2020-08-16 19:58:26 -0400

    sets heroku PORT again

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT ||  3333, ()=> {
-  console.log("Back-end started! 🚀🚀 ")
-});
+// app.listen(process.env.PORT ||  3333, ()=> {
+//   console.log("Back-end started! 🚀🚀 ")
+// });
+
+app.listen(process.env.PORT);
